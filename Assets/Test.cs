afe401950590293134e0faef8ec016f6ba40f43b
triangle-apple <102868413+triangle-apple@users.noreply.github.com>
2022-06-04 18:46:39 +0900

    Initial commit

---
+++ Assets/Test.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 0から順に数を代入する、宣言もする
+        int[] array = { 1, 2, 3, 4, 5 };
+        // 配列の各要素を順に表示
+       // for (int i = 0; i < array.Length; i++)
+        {
+          //  Debug.Log(array[i]);
+        }
+        // 配列の要素を逆順に表示
+        for (int i = 4; i < array.Length; i--)
+        {
+            Debug.Log(array[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
